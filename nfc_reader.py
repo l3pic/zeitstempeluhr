dba6ed96011803bb857b014b64eacfc04f3aa17a
l3pic <43809826+l3pic@users.noreply.github.com>
2023-08-11 16:55:19 +0200

    Add files via upload

--- nfc_reader.py
+++ nfc_reader.py
@@ -3,12 +3,42 @@ import sys
 sys.path.append('/var/py/')
 
 import RPi.GPIO as GPIO
-from mfrc522 import SimpleMFRC522
+from MFRC522 import MFRC522
 import mysql.connector
 from datetime import datetime
 import time
 import Adafruit_CharLCD as LCD
 
+KEY = [0xFF,0xFF,0xFF,0xFF,0xFF,0xFF]
+BLOCK_ADDRS = [8, 9, 10]
+
+def read_no_block(reader):
+    (status, TagType) = reader.MFRC522_Request(reader.PICC_REQIDL)
+    if status != reader.MI_OK:
+        return None, None
+    (status, uid) = reader.MFRC522_Anticoll()
+    if status != reader.MI_OK:
+        return None, None
+    id = uid_to_num(uid)
+    reader.MFRC522_SelectTag(uid)
+    status = reader.MFRC522_Auth(reader.PICC_AUTHENT1A, 11, KEY, uid)
+    data = []
+    text_read = ''
+    if status == reader.MI_OK:
+        for block_num in BLOCK_ADDRS:
+            block = reader.MFRC522_Read(block_num)
+            if block:
+                data += block
+        if data:
+            text_read = ''.join(chr(i) for i in data)
+    reader.MFRC522_StopCrypto1()
+    return id, text_read
+
+def uid_to_num(uid):
+    n = 0
+    for i in range(0, 5):
+        n = n * 256 + uid[i]
+    return n
 
 GPIO.setwarnings(False)
 reading = bool(True)
@@ -47,59 +77,60 @@ sql_insert_zeiten = "INSERT INTO zeiten (datetime, nfc_uid, nfc_tag, int_datetim
 sql_check_uid = "SELECT * FROM user WHERE nfc_uid = %s"
 sql_create_user = "INSERT INTO user (username, passwd, sec_lvl, nfc_uid) VALUES(%s, %s, %s, %s)"
 
-reader = SimpleMFRC522()
+reader = MFRC522(spd=8000000)
 while reading:
-        try:
-                id, text = reader.read()
-                print(id)
-                text = text.rstrip()
-                print(text)
-                now = datetime.now()
-                dt_string = now.strftime("%H:%M %d-%m-%Y")
-                dt_int = int(now.strftime("%Y%m%d%H%M"))
-                print(dt_string)
-                print(dt_int)
-                if id and text:
-                        val_check_uid = (id, )
-                        mycursor.execute(sql_check_uid, val_check_uid)
-                        result = mycursor.fetchall()
-                        for row in result:
-                            db_tag = row[1]
+    try:
+        id, text = read_no_block(reader)
+        if not id is None:
+            print(id)
+            text = text.rstrip()
+            print(text)
+            now = datetime.now()
+            dt_string = now.strftime("%H:%M %d-%m-%Y")
+            dt_int = int(now.strftime("%Y%m%d%H%M"))
+            print(dt_string)
+            print(dt_int)
+            if id and text:
+                val_check_uid = (id, )
+                mycursor.execute(sql_check_uid, val_check_uid)
+                result = mycursor.fetchall()
+                for row in result:
+                    db_tag = row[1]
 
-                        if len(result) == 0:
-                            val_create_user = (text, "", 1, id)
-                            mycursor.execute(sql_create_user, val_create_user)
-                            mydb.commit()
-                            lcd.message('Neuer Chip\nwurde erkannt!')
-                            GPIO.output(green_led, GPIO.LOW)
-                            time.sleep(3)
-                            GPIO.output(green_led, GPIO.HIGH)
-                            lcd.clear()
-                            time.sleep(1)
-                        else:
-                            if db_tag == text:
-                                val_insert_zeiten = (dt_string, id, text, dt_int)
-                                mycursor.execute(sql_insert_zeiten, val_insert_zeiten)
-                                mydb.commit()
-                                lcd.message('Eintrag erstellt\n%s' % (dt_string))
-                                GPIO.output(green_led, GPIO.LOW)
-                                time.sleep(3)
-                                GPIO.output(green_led, GPIO.HIGH)
-                                lcd.clear()
-                                time.sleep(1)
-                            else:
-                                reader.write(db_tag)
-                                lcd.message('Tag erfolgreich\nueberschrieben')
-                                GPIO.output(green_led, GPIO.LOW)
-                                time.sleep(3)
-                                GPIO.output(green_led, GPIO.HIGH)
-                                lcd.clear()
-                                time.sleep(1)
+                if len(result) == 0:
+                    val_create_user = (text, "", 1, id)
+                    mycursor.execute(sql_create_user, val_create_user)
+                    mydb.commit()
+                    lcd.message('Neuer Chip\nwurde erkannt!')
+                    GPIO.output(green_led, GPIO.LOW)
+                    time.sleep(3)
+                    GPIO.output(green_led, GPIO.HIGH)
+                    lcd.clear()
+                    time.sleep(1)
                 else:
-                        lcd.message('Fehler bitte\nerneut versuchen')
-                        GPIO.output(red_led, GPIO.HIGH)
-                        time.sleep(2)
+                    if db_tag == text:
+                        val_insert_zeiten = (dt_string, id, text, dt_int)
+                        mycursor.execute(sql_insert_zeiten, val_insert_zeiten)
+                        mydb.commit()
+                        lcd.message('Eintrag erstellt\n%s' % (dt_string))
+                        GPIO.output(green_led, GPIO.LOW)
+                        time.sleep(3)
+                        GPIO.output(green_led, GPIO.HIGH)
                         lcd.clear()
-                        GPIO.output(red_led, GPIO.LOW)
-        finally:
-            print("")
+                        time.sleep(1)
+                    else:
+                        reader.write(db_tag)
+                        lcd.message('Tag erfolgreich\nueberschrieben')
+                        GPIO.output(green_led, GPIO.LOW)
+                        time.sleep(3)
+                        GPIO.output(green_led, GPIO.HIGH)
+                        lcd.clear()
+                        time.sleep(1)
+            else:
+                    lcd.message('Fehler bitte\nerneut versuchen')
+                    GPIO.output(red_led, GPIO.HIGH)
+                    time.sleep(2)
+                    lcd.clear()
+                    GPIO.output(red_led, GPIO.LOW)
+    finally:
+        time.sleep(0.1)
